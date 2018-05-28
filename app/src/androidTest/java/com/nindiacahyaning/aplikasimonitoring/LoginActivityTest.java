4b26f33f89bcbe8c37aee76d1f54b3d9aa4464ca
Putrinindiaa <already.nindia@yahoo.com>
2018-05-28 20:31:51 +0700

    First Commit
    First Commit

---
+++ app/src/androidTest/java/com/nindiacahyaning/aplikasimonitoring/LoginActivityTest.java
@@ -0,0 +1,10 @@
+package com.nindiacahyaning.aplikasimonitoring;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Nindia Putri on 21/05/2018.
+ */
+public class LoginActivityTest {
+
+}
---
+++ app/src/main/java/com/nindiacahyaning/aplikasimonitoring/ForgoPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.nindiacahyaning.aplikasimonitoring;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgoPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgo_password);
+    }
+}
